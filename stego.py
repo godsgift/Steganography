4a9705fee35f8e85f068730e8900559f1cd68717
godsgift <eltonsia@yahoo.com>
2015-09-30 20:47:24 -0700

    added stuff

--- stego.py
+++ stego.py
@@ -1,11 +1,19 @@
 from PIL import Image
 import PIL.ImageOps
+import os
 
-def fileToBinary():
+def secretFile():
+	bits = ""
+	#open file as read and binary mode
 	file = open("Secret.txt", "rb")
+	#convert whatever is in the file into bytes
 	readFile = bytearray(file.read())
-	for bits in readFile:
-		print bits
+	#convert the bytes into bits
+	for bit in readFile:
+		#store all the bits into a string
+		bits += bin(bit)[2:].zfill(8)
+	lbits = list(bits)
+	return lbits
 	#convert = "".join(format(x, 'b').zfill(8) for x in bytearray(readFile))
 
 def stringToBinary():
@@ -13,26 +21,41 @@ def stringToBinary():
 	convert = "".join(format(x, 'b').zfill(8) for x in bytearray(message))
 	print convert
 
-def coverImage():
+def compare(hiddenFile, coverFile):
+	coverImageSize = os.path.getsize(coverFile)
+	print "Your cover image size is: %d" %coverImageSize
+	hiddenFileSize = os.path.getsize(hiddenFile)
+	print "Your hidden image size is: %d" %hiddenFileSize
 
+def stego():
 	#Open image
-	coverImage = Image.open("trollface.bmp")
+	coverImage = Image.open("trollface2.bmp")
 	#convert the image into RGBA
 	rgba_img = coverImage.convert('RGBA')
 	#grab the size of image and store it into width and height
 	width, height = coverImage.size
 
+	secret = secretFile)
+
+	#imgSize = width*height
+	#print imgSize
 	for x in range(width):
 		for y in range(height):
+			#grab the rgba value of each pixel
 			r, g, b, a = rgba_img.getpixel((x, y))
+			#convert each rgba value into binary
 			red = list(bin(r)[2:].zfill(8))
-			green = bin(g)[2:].zfill(8)
-			blue = bin(b)[2:].zfill(8)
-			alpha = bin(a)[2:].zfill(8)
-			print red
-			print green
-			print blue
-			print alpha
+			green = list(bin(g)[2:].zfill(8))
+			blue = list(bin(b)[2:].zfill(8))
+			alpha = list(bin(a)[2:].zfill(8))
+			#put each pixels rgba into an array
+			rgba_array = [red, green, blue, alpha]
+			#grab each pixels rgba
+			for epix in rgba_array:
+				#print epix[7]
+				return
 
-fileToBinary()
-#coverImage()
+if __name__ == "__main__":	
+	secretFile()
+	stego()
+	#compare("trollface.bmp", "Secret.txt")
