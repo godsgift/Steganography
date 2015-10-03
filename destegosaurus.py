0704a4454f732cbf815c4e11769d7042f09b7925
Baus <eltonsia@yahoo.com>
2015-10-03 07:00:59 -0400

    Added header on both encoding and decoding, however decoding messes up the very last part of the message

--- destegosaurus.py
+++ destegosaurus.py
@@ -2,11 +2,12 @@ from PIL import Image
 import PIL.ImageOps
 import sys
 import array
-
+import binascii
 
 def pixyGrabber():
-	global secretMessageSize
+	global secretMessageSizeAscii
 	global rgb_array
+	global tat
 	rgb_array = ""
 	secretImage = Image.open("test.bmp")
 	rgb_img = secretImage.convert('RGB')
@@ -18,7 +19,12 @@ def pixyGrabber():
 	secretMessageSize = ""
 	secretMessageIndex = 0
 	nullCounter = 1
-
+	temp = ""
+	binStringSecretMsg = ""
+	secretMessageSizeAscii = ""
+	tet = 0
+	cCounter = 0
+	tat = ""
 	#iterate through all the pixels
 	for x in range(width):
 		for y in range(height):
@@ -29,13 +35,12 @@ def pixyGrabber():
 			blueBinString = str(bin(b)[2:].zfill(8))[7]
 
 			rgb_array += redBinString + greenBinString + blueBinString
-			#group the string in chunks of 8 so that its in binary
 	
+	#group the string in chunks of 8 so that its in binary
 	group = [rgb_array[i:i +8] for i in range(0, len(rgb_array), 8)]
-	temp = ""
-	ss = ""
+
 	for i in group:
-		
+
 		temp += i
 		if (i == nullCondition and nullCounter == 1):
 			fileName_bytes = temp[0:len(temp) - 8]
@@ -45,55 +50,44 @@ def pixyGrabber():
 		elif (i == nullCondition and nullCounter == 2):
 			secretMessageSize = temp[0:len(temp) - 8]
 			secretMessageSizeList = list(secretMessageSize)
-			cc = 0
+			counter = 0
 			for j in range(len(secretMessageSizeList)/8):
 				for i in range(0, 8):
-					ss += secretMessageSizeList[cc]
-					cc += 1
-				print ss
-				
-				#print chr(test)
-				ss = ""
-			print ss
+					binStringSecretMsg += secretMessageSizeList[counter]
+					counter += 1
+				secretMessageSizeAscii += ''.join(chr(int(binStringSecretMsg[i:i+8], 2)) for i in xrange(0, len(binStringSecretMsg), 8))
+				binStringSecretMsg = ""
+			tet =  int(secretMessageSizeAscii)
+			print tet
+			nullCounter += 1
 
-			nullCounter += 3
-
-# 		elif secretMessageIndex <= int(secretMessageSize, 2):
-# 			#grab the rgb values from the x and y values of the pixels
-# 			r, g, b = rgb_img.getpixel((x, y))
-# 			#change rgb into binary then grab the last bit and turn it into a string
-# 			redBinString = str(bin(r)[2:].zfill(8))[7]
-# 			greenBinString = str(bin(g)[2:].zfill(8))[7]
-# 			blueBinString = str(bin(b)[2:].zfill(8))[7]
-# 			#combine all the rgb string values and put it into rgb_array
-# 			rgb_array += redBinString + greenBinString + blueBinString
+		elif(nullCounter == 3):
+			for k in range(tet):
+				tat += group[cCounter+k]
+			return
+			nullCounter += 1
+		cCounter += 1
 			
-# 			secretMessageIndex += 3
-# 		else:
-# 			return
 
-# def newFile():
-# 	secretData = ""
-# 	byteAray = []
+def newFile():
+	secretData = ""
+	byteAray = []
 
-# 	#iterate through the size of the secret message and add the last bits into secretData
-# 	for i in range(int(secretMessageSize)):
-# 		secretData += rgb_array[i]
+	#iterate through the size of the secret message and add the last bits into secretData
+	for i in range(int(secretMessageSizeAscii)):
+		secretData += tat[i]
 
-# 	print len(secretData)
-# 	print secretData
+	newByteArray = []
+	#
+	for i in range (0, len(secretData)/8):
+		newByteArray.append(int(secretData[i*8:(i+1) * 8], 2))
 
-# 	newByteArray = []
-# 	#
-# 	for i in range (0, len(secretData)/8):
-# 		newByteArray.append(int(secretData[i*8:(i+1) * 8], 2))
+	byteArray = array.array('B', newByteArray).tostring()
+	secretMessage = bytearray(byteArray)
 
-# 	byteArray = array.array('B', newByteArray).tostring()
-# 	secretMessage = bytearray(byteArray)
-
-# 	createFile = open('test.txt', 'w')
-# 	createFile.write(secretMessage)
+	createFile = open('test.txt', 'w')
+	createFile.write(secretMessage)
 
 if __name__ == "__main__":
 	pixyGrabber()
-	#newFile()
+	newFile()
